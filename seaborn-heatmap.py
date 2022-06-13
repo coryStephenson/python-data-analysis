03e7a805b18b51c199d7fdce40f1a45c21dbc424
Cory Stephenson <34756469+coryStephenson@users.noreply.github.com>
2022-06-12 23:38:40 -0400

    Fixed syntax errors

--- seaborn-heatmap.py
+++ seaborn-heatmap.py
@@ -5,12 +5,12 @@ import numpy as np
 from bokeh.plotting import figure, show
 
 # Data
-fruits = [ <<apple>>, <<banana>>, <<grape>>, <<strawberry>>, <<papaya>> ]
-stage = [ <<ripe>>, <<unripe>>, <<rotten>> ]
+fruits = [ "apple", "banana", "grape", "strawberry", "papaya" ]
+stage = [ "ripe", "unripe", "rotten" ]
 
 data_fruits = np.random.choice(fruits, size=100)
 data_stage = np.random.choice(stage, size=100)
-ct = pd.crosstable(data_fruits, data_stage)
+ct = pd.crosstab(data_fruits, data_stage)
 
 # Seaborn
 sns.heatmap(ct)
